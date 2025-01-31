4b88541b21e1f3c9ffd65b98c939bc85998aa8c2
David Kjellstrand <david.kjellstrand@gmail.com>
2025-01-31 18:07:11 +0100

    Add deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  'base': '/jumping-game/', // TODO: Ändra till ert reponamn här
   plugins: [vue()],
 })
